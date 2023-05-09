3e4a757c4baca0b265b8d810382f487748f34653
Dezolutions <mars92851@gmail.com>
2023-05-10 01:28:44 +0300

    layout build, mobile adaptation

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,27 +1,39 @@
 import React from 'react'
 import TransactionForm from '../TransactionForm/TransactionForm'
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 import './main.css'
+
 const Main :React.FC = () => {
   return (
-    <main className='main'>
-      <div className="main__heading-block">
-        <h1 className="heading-block__title">Send Crypto <br /> across the world</h1>
-        <p className="heading-block__subtitle">Explore the crypto world. Buy and sell cryptocurrencies easily.</p>
-        <button className="heading-block__wallet-btn">Connect Wallet</button>
-        <div className="heading-block__items">
-          <div className="item">Reliability</div>
-          <div className="item">Security</div>
-          <div className="item">Ethereum</div>
-          <div className="item">Web 3.0</div>
-          <div className="item">Blockchain</div>
-          <div className="item">Exchange</div>
-        </div>
-      </div>
-      <div className="main__form-block">
-        <div className='eth-card white-glassmorphism card-item'></div>
-        <TransactionForm/>
-      </div>
-    </main>
+    <div className='container'>
+      <main className='main'>
+          <div className="main__heading-block">
+            <h1 className="heading-block__title">Send Crypto <br /> across the world</h1>
+            <p className="heading-block__subtitle">Explore the crypto world. Buy and sell cryptocurrencies easily.</p>
+            <button className="heading-block__wallet-btn">Connect Wallet</button>
+            <div className="heading-block__items">
+              <div className="item">Reliability</div>
+              <div className="item">Security</div>
+              <div className="item">Ethereum</div>
+              <div className="item">Web 3.0</div>
+              <div className="item">Blockchain</div>
+              <div className="item">Exchange</div>
+            </div>
+          </div>
+          <div className="main__form-block">
+            <div className='eth-card white-glassmorphism card-item'>
+              <div className='eth-card__eth-icon'><SiEthereum fontSize={26} color="#fff" /></div>
+              <div className='eth-card__info-icon'><BsInfoCircle fontSize={20} color="#fff" /></div>
+              <div className='eth-card__info-block'>
+                <p className='eth-card__address'>...</p>
+                <p className='eth-card__currency'>Ethereum</p>
+              </div>
+            </div>
+            <TransactionForm/>
+          </div>
+      </main>
+    </div>
   )
 }
 
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
+
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
 import './navbar.css'
 
 const Navbar :React.FC = () => {
+  const [toggleMenu, setToggleMenu] = React.useState<boolean>(false);
+
   return (
     <header className='header'>
-      <div className="header-logo">
-        <img className='header-logo__item' src={logo} alt="logotype" />
+      <div className='container'>
+        <div className='header-grid'>
+          <div className="header-logo">
+            <img className='header-logo__item' src={logo} alt="logotype" />
+          </div>
+          <nav className='header__nav-block'>
+            <ul className={toggleMenu ? "header__nav-list active" : "header__nav-list"}>
+              <li><a className="nav-item" href='#'>Wallets</a></li>
+              <li><a className="nav-item" href='#'>Exchange</a></li>
+              <li><a className="nav-item" href='#'>Currencies</a></li>
+            </ul>
+            {toggleMenu
+              ? <div><AiOutlineClose className='header__burger-close' fontSize={28} onClick={() => setToggleMenu(false)}/></div>
+              : <div><HiMenuAlt4 className='header__burger-menu' fontSize={28} onClick={() => setToggleMenu(true)}/></div>
+            }
+          </nav>
+        </div>
       </div>
-      <nav>
-        <ul className="header__nav-list">
-          <li><a className="nav-item" href='#'>Wallets</a></li>
-          <li><a className="nav-item" href='#'>Exchange</a></li>
-          <li><a className="nav-item" href='#'>Currencies</a></li>
-        </ul>
-      </nav>
     </header>
   )
 }
